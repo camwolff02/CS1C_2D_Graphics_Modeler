23fe5dfbd3b4c2c48c0b34174b422f7e6f276ba6
Christopher Schrader <christopherschrader@Christophers-MacBook-Pro.local>
2021-12-07 13:14:13 -0800

    Modified addLines

--- QTPROJECT/addLines.cpp
+++ QTPROJECT/addLines.cpp
@@ -1,6 +1,8 @@
 #include "addLines.h"
 #include "ui_addLines.h"
 
+#include "line.h"
+
 addLines::addLines(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::addLines)
@@ -12,3 +14,11 @@ addLines::~addLines()
 {
     delete ui;
 }
+
+void addLines::on_buttonBox_accepted()
+{
+    //Line is ready to be made
+    QPoint point1(ui->x1->text().toInt(), ui->y1->text().toInt());
+    QPoint point2(ui->x2->text().toInt(), ui->y2->text().toInt());
+}
+
